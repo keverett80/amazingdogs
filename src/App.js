1745aa58320f7863f620542309c91203547c9253
MyTeamTweets <keverett1980@gmail.com>
2021-06-19 11:28:43 -0400

    added auth

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
 
 function App() {
   return (
